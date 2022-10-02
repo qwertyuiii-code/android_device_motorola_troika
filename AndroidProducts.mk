35c9139431439b31c627b51a95863c09c35032b4
qwertyuiii-code <soorprp@ya.ru>
2022-10-02 22:01:58 +0500

    troika: bringup PE dt

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_troika.mk
+    $(LOCAL_DIR)/aosp_troika.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_troika-user \
-    lineage_troika-userdebug \
-    lineage_troika-eng
+    aosp_troika-user \
+    aosp_troika-userdebug \
+    aosp_troika-eng
